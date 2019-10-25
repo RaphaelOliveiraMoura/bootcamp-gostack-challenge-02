236012702ab65abe54c9268d78d1021ab4756085
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-25 09:07:40 -0300

    refact: Formmat student validation flux

--- src/app/validators/StudentValidator.js
+++ src/app/validators/StudentValidator.js
@@ -21,20 +21,20 @@ class StudentValidator {
     });
 
     try {
-      await schema.validate(request.body, {
-        abortEarly: false,
-      });
-
-      const emailAlreadyInUse = await Student.findOne({
-        where: { email: request.body.email },
-      });
-      if (emailAlreadyInUse)
-        return response.status(401).json({ error: 'Student already exists' });
-
-      return next();
+      await schema.validate(request.body, { abortEarly: false });
     } catch ({ errors }) {
       return response.status(401).json({ errors });
     }
+
+    const emailAlreadyInUse = await Student.findOne({
+      where: { email: request.body.email },
+    });
+
+    if (emailAlreadyInUse) {
+      return response.status(401).json({ error: 'Student already exists' });
+    }
+
+    return next();
   }
 
   async update(request, response, next) {
@@ -47,35 +47,36 @@ class StudentValidator {
     });
 
     try {
-      await schema.validate(request.body, {
-        abortEarly: false,
-      });
-
-      const { id } = request.params;
-
-      const student = await Student.findOne({
-        where: { id },
-      });
-
-      if (!student)
-        return response.status(401).json({ error: 'Student does not exist' });
-
-      request.student = student;
-
-      if (request.body.email) {
-        const studentAlreadyExists = await Student.findOne({
-          where: { email: request.body.email },
-        });
-        if (studentAlreadyExists)
-          return response.status(401).json({
-            error: 'You cannot set this email, because it already in use',
-          });
-      }
-
-      return next();
+      await schema.validate(request.body, { abortEarly: false });
     } catch ({ errors }) {
       return response.status(401).json({ errors });
     }
+
+    const { id } = request.params;
+
+    const student = await Student.findOne({
+      where: { id },
+    });
+
+    if (!student) {
+      return response.status(401).json({ error: 'Student does not exist' });
+    }
+
+    request.student = student;
+
+    if (request.body.email) {
+      const studentAlreadyExists = await Student.findOne({
+        where: { email: request.body.email },
+      });
+
+      if (studentAlreadyExists) {
+        return response.status(401).json({
+          error: 'You cannot set this email, because it already in use',
+        });
+      }
+    }
+
+    return next();
   }
 }
 
