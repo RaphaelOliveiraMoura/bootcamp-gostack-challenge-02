d2f6cd0a28108f7468f269453f748f813aa51faa
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-11-26 15:57:22 -0300

    feat: @web/List of plans

--- web/src/pages/Plans/index.js
+++ web/src/pages/Plans/index.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Table, EditButton, DeleteButton } from './styles';
+
+import Button from '~/components/Button';
+import TitleContainer from '~/components/TitleContainer';
+
+import api from '~/services/api';
+
+import { formatPrice } from '~/util/format';
 
 export default function Plans() {
-  return <div />;
+  const [plans, setPlans] = useState([]);
+
+  useEffect(() => {
+    async function loadPlans() {
+      const response = await api.get('/plans');
+
+      const data = response.data.map(plan => ({
+        ...plan,
+        formattedPrice: formatPrice(plan.price),
+      }));
+
+      setPlans(data);
+    }
+    loadPlans();
+  }, []);
+
+  return (
+    <Container>
+      <TitleContainer>
+        <h1>Gerenciando planos</h1>
+        <Link to="/plans/create">
+          <Button>CADASTRAR</Button>
+        </Link>
+      </TitleContainer>
+      <Table>
+        <thead>
+          <tr>
+            <th>TÍTULO</th>
+            <th>DURAÇÃO</th>
+            <th>VALOR p/MÊS</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {plans.map(plan => (
+            <tr key={String(plan.id)}>
+              <td>{plan.title}</td>
+              <td>{plan.duration}</td>
+              <td>{plan.formattedPrice}</td>
+              <td>
+                <div className="options">
+                  <EditButton type="button">editar</EditButton>
+                  <DeleteButton type="button">apagar</DeleteButton>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
--- web/src/pages/Plans/styles.js
+++ web/src/pages/Plans/styles.js
@@ -1,3 +1,49 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import Button from '~/components/Button';
+import TableWrapper from '~/components/Table';
+
+export const Container = styled.div`
+  margin: 30px auto;
+  max-width: 900px;
+  padding: 0 8px;
+`;
+
+export const Table = styled(TableWrapper)`
+  text-align: center;
+
+  th:nth-child(1),
+  td:nth-child(1) {
+    text-align: left;
+  }
+
+  @media (max-width: 600px) {
+    th:nth-child(2),
+    td:nth-child(2) {
+      display: none;
+    }
+  }
+
+  @media (max-width: 435px) {
+    th:nth-child(3),
+    td:nth-child(3) {
+      display: none;
+    }
+  }
+`;
+
+export const EditButton = styled(Button).attrs({
+  background: '#fff',
+})`
+  font-size: 15px;
+  color: #4d85ee;
+  font-weight: normal;
+`;
+
+export const DeleteButton = styled(Button).attrs({
+  background: '#fff',
+})`
+  font-size: 15px;
+  color: #de3b3b;
+  font-weight: normal;
+`;
