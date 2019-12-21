f0ae3d92aa8fea37e490abdae69b8a3dbf0fb2d9
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-12-21 19:01:10 -0300

    feat: Add sentry to catch errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,18 +3,26 @@ import express from 'express';
 import { resolve } from 'path';
 import cors from 'cors';
 
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import '~/database';
 import routes from '~/routes';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
 
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors({ exposedHeaders: 'total_pages' }));
     this.server.use(express.json());
     this.server.use(
@@ -23,8 +31,16 @@ class App {
     );
   }
 
+  exceptionHandler() {
+    this.server.use(async (error, request, response, next) => {
+      const errors = await new Youch(error, request).toJSON();
+      return response.status(500).json(errors);
+    });
+  }
+
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
